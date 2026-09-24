90b7868ae51a4ce2b09381e99d8011fb8aa86bd8
Teles Nascimento <darlyssonteles98@gmail.com>
2026-09-24 15:32:09 -0300

    Handle auditor provider lookup

--- extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/audit/AuditingGenerationSupport.java
+++ extensions/hibernate-orm/runtime/src/main/java/io/quarkus/hibernate/orm/runtime/audit/AuditingGenerationSupport.java
@@ -1,6 +1,7 @@
 package io.quarkus.hibernate.orm.runtime.audit;
 
 import io.quarkus.arc.Arc;
+import io.quarkus.arc.InjectableInstance;
 import io.quarkus.arc.InstanceHandle;
 import io.quarkus.hibernate.orm.audit.CurrentAuditorProvider;
 
@@ -10,11 +11,17 @@ final class AuditingGenerationSupport {
     }
 
     static Object currentAuditorOrCurrentValue(Object currentValue) {
-        InstanceHandle<CurrentAuditorProvider> provider = Arc.container().instance(CurrentAuditorProvider.class);
-        if (!provider.isAvailable()) {
+        InjectableInstance<CurrentAuditorProvider> providers = Arc.container().select(CurrentAuditorProvider.class);
+        if (providers.isUnsatisfied()) {
             return currentValue;
         }
-        String currentAuditor = provider.get().currentAuditor();
-        return currentAuditor != null ? currentAuditor : currentValue;
+        if (providers.isAmbiguous()) {
+            throw new IllegalStateException(
+                    "Multiple CurrentAuditorProvider beans were found. At most one provider can be active.");
+        }
+        try (InstanceHandle<CurrentAuditorProvider> handle = providers.getHandle()) {
+            String currentAuditor = handle.get().currentAuditor();
+            return currentAuditor != null ? currentAuditor : currentValue;
+        }
     }
 }
